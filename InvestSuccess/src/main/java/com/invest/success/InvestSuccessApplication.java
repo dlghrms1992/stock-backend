29ac41ada963b3d55a0d23159be60eed829d26be
dlghrms1992 <dlghrms1992@naver.com>
2021-06-08 22:00:01 +0900

    backend start

---
+++ InvestSuccess/src/main/java/com/invest/success/InvestSuccessApplication.java
@@ -0,0 +1,13 @@
+package com.invest.success;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestSuccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestSuccessApplication.class, args);
+	}
+
+}
---
+++ InvestSuccess/src/test/java/com/invest/success/InvestSuccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invest.success;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestSuccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
